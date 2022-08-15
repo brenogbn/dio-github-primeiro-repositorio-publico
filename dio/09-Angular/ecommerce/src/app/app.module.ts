5f840932c8b6f7a789cbb62112d7703b03a4c74c
BrenoGBN <breno@neves.dev.br>
2022-08-14 21:34:35 -0300

    Ecommerce

---
+++ dio/09-Angular/ecommerce/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { BookstoreAppComponent } from './components/shared/bookstore-app/bookstore-app.component';
+import { FiltersComponent } from './components/shared/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './components/shared/bookstore-app/product-list/product-list.component';
+import { AppHeaderComponent } from './components/shared/app-header/app-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    BookstoreAppComponent,
+    FiltersComponent,
+    ProductListComponent,
+    AppHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
